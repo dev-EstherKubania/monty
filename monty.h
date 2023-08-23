3dc5d5550dadc8ebf2cc8f9c38ce1bb60eea264e
dev-EstherKubania <eskury@gmail.com>
2023-08-23 07:21:24 -0700

    header file

---
+++ monty.h
@@ -0,0 +1,54 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+    int n;
+    struct stack_s *prev;
+    struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+    char *opcode;
+    void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+/* Stack operations */
+void push(stack_t **stack, int value);
+int pop(stack_t **stack);
+
+/* Opcode execution */
+void execute(const char *filename);
+
+/* Opcode processing functions */
+void process_push_pop(stack_t **stack, int arg, unsigned int line_number);
+void process_print(stack_t **stack, unsigned int line_number);
+void process_pall(stack_t **stack, unsigned int line_number);
+void process_pint(stack_t **stack, unsigned int line_number);
+void process_swap(stack_t **stack, unsigned int line_number);
+void process_add(stack_t **stack, unsigned int line_number);
+void process_nop(stack_t **stack, unsigned int line_number);
+
+#endif /* MONTY_H */
+
